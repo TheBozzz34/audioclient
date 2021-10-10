963fa508d9dcafc8e943e2007038d9310ec2bd59
Ethan <lskywalker416@gmail.com>
2021-10-09 23:18:46 -0600

    Add files

---
+++ src/main/java/company/fourleafclover/Main.java
@@ -0,0 +1,24 @@
+package company.fourleafclover;
+
+import io.sentry.Sentry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main {
+    private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        Sentry.init(options -> {
+            options.setDsn("https://8515bca5b990450bbc0ce4ec5935d2c0@o561860.ingest.sentry.io/5699748");
+            // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.setTracesSampleRate(1.0);
+            // When first trying Sentry it's good to see what the SDK is doing:
+            options.setDebug(false);
+        });
+        LOGGER.info("Initialized Sentry");
+    }
+
+}
